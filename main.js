b1dfc500b43d9246d86ebc389aa760748400e7e4
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-11-08 17:20:23 +0500

    -

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import { Story } from "./components/StoryComponent.js";
-import { reload } from "./libs/utils.js";
-
-
+import { reload } from "./utils/reload.js";
 
 
 fetch('http://localhost:3001/stories')
-.then(response => response.json())
-.then(data => reload(data, document.querySelector('.story__list'), Story))
+    .then(response => response.json())
+    .then(data => reload(data, document.querySelector('.story__list'), Story))
--- utils/reload.js
+++ utils/reload.js
@@ -1,8 +1,8 @@
 export function reload(arr, place, Component) {
-place.innerHTML = ""
+    place.innerHTML = ""
     for (let item of arr) {
         let elem = Component(item)
-        
+
 
         place.append(elem)
     }
